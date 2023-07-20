f7d5bf098a9784956879574289e6ed15a96c3be4
aort <artashesano@gmail.com>
2023-07-20 16:12:33 +0400

    Make tabs in TabBar selectedable

--- src/components/TabBar.tsx
+++ src/components/TabBar.tsx
@@ -1,14 +1,28 @@
+'use client';
 import styles from '@/styles/components/TabBar.module.scss';
 import type { Tab as TabType } from '@/types';
+import { useState } from 'react';
 import Tab from './Tab';
 
 const tabs: TabType[] = ['posts', 'albums', 'todos'];
 
 const TabBar: React.FC<{}> = () => {
+  const [selectedTab, setSelectedTab] = useState<TabType>('posts');
+
+  const handleTabChange = (tab: TabType) => {
+    setSelectedTab(tab);
+  };
+
   return (
     <div className={styles.container}>
       {tabs.map((tab) => (
-        <Tab key={tab}>{tab}</Tab>
+        <Tab
+          onClick={() => handleTabChange(tab)}
+          isSelected={tab === selectedTab}
+          key={tab}
+        >
+          {tab}
+        </Tab>
       ))}
     </div>
   );
