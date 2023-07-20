f7bcef194d6615d8499ce8dd30a2e808639b8ba2
aort <artashesano@gmail.com>
2023-07-20 18:14:22 +0400

    Add all three pages and env variables

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -2,10 +2,10 @@ import Post from '@/components/Post';
 import '@/styles/utils/classes.scss';
 import type { Post as PostType } from '@/types';
 
+const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
+
 const Page = async () => {
-  const posts: PostType[] = await (
-    await fetch('https://jsonplaceholder.typicode.com/posts')
-  ).json();
+  const posts: PostType[] = await (await fetch(`${baseUrl}posts`)).json();
 
   return (
     <div className="column align_items_center gap_4">
