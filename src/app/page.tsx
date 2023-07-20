3533211b5b3a483510c3a4bbb4ceea041371ee3a
aort <artashesano@gmail.com>
2023-07-20 14:41:29 +0400

    Cleanup page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
-import styles from '@/styles/index.module.scss';
-
 const Page = () => {
-  return <h1 className={styles.textColor9}>Hello World</h1>;
+  return <div></div>;
 };
 
 export default Page;
