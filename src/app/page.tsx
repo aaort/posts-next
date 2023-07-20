d5ba808d862927815f2641271fc4f4844e8e9ce3
aort <artashesano@gmail.com>
2023-07-20 16:12:45 +0400

    Render posts in page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,19 @@
-const Page = () => {
-  return <div></div>;
+import Post from '@/components/Post';
+import '@/styles/utils/classes.scss';
+import type { Post as PostType } from '@/types';
+
+const Page = async () => {
+  const posts: PostType[] = await (
+    await fetch('https://jsonplaceholder.typicode.com/posts')
+  ).json();
+
+  return (
+    <div className="column align_items_center gap_4">
+      {posts.map((post) => (
+        <Post key={post.id} post={post} />
+      ))}
+    </div>
+  );
 };
 
 export default Page;
