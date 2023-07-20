7da346812249638c3c4b25fbed5b674b270bc838
aort <artashesano@gmail.com>
2023-07-20 19:33:30 +0400

    Implement todos page

--- src/app/todos/page.tsx
+++ src/app/todos/page.tsx
@@ -1,5 +1,19 @@
-const Page = () => {
-  return <h1>Todos page</h1>;
+import Album from '@/components/Album';
+import '@/styles/utils/classes.scss';
+import type { Todo as TodoType } from '@/types';
+
+const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
+
+const Page = async () => {
+  const todos: TodoType[] = await (await fetch(`${baseUrl}todos`)).json();
+
+  return (
+    <div className="column align_items_center gap_4">
+      {todos.map((todo) => (
+        <Album key={todo.id} album={todo} />
+      ))}
+    </div>
+  );
 };
 
 export default Page;
