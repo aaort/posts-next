737e5a35c0b28fbdc0978550f8229b9893b9138c
aort <artashesano@gmail.com>
2023-07-20 16:11:32 +0400

    Add types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,13 @@
 type Tab = 'posts' | 'albums' | 'todos';
 
-export type { Tab };
+type Common = {
+  id: number;
+};
+
+type Post = Common & {
+  userId: number;
+  title: string;
+  body: string;
+};
+
+export type { Tab, Post };
