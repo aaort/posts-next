3f78333fa0401d4f09ec84f9f0ce8d70e90aa1f4
aort <artashesano@gmail.com>
2023-07-20 16:11:59 +0400

    Implement Tab component

--- src/components/Tab.tsx
+++ src/components/Tab.tsx
@@ -1,9 +1,21 @@
 import styles from '@/styles/components/Tab.module.scss';
 
-type Props = React.PropsWithChildren;
+type Props = React.PropsWithChildren & {
+  isSelected: boolean;
+  onClick: () => void;
+};
 
-const Tab: React.FC<Props> = ({ children }) => {
-  return <div className={styles.container}>{children}</div>;
+const Tab: React.FC<Props> = ({ children, isSelected, onClick }) => {
+  return (
+    <div
+      onClick={onClick}
+      className={`${styles.container} ${
+        isSelected ? styles.selected_container : ''
+      }`}
+    >
+      {children}
+    </div>
+  );
 };
 
 export default Tab;
