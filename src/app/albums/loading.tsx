e5fd527cfdde1e322fd7369a683f5fc9ab3b422f
aort <artashesano@gmail.com>
2023-07-21 16:32:14 +0400

    Add proper loading pages for albums and todos

--- src/app/albums/loading.tsx
+++ src/app/albums/loading.tsx
@@ -1,7 +1,15 @@
-import LoadingSpinner from '@/components/Loading';
+import styles from '@/styles/components/Card.module.scss';
+import '@/styles/utils/classes.scss';
 
 const Loading: React.FC<{}> = () => {
-  return <LoadingSpinner />;
+  return (
+    <div className="column align_items_center gap_4">
+      <div className={`${styles.container_lighten}`}></div>
+      <div className={`${styles.container_lighten}`}></div>
+      <div className={`${styles.container_lighten}`}></div>
+      <div className={`${styles.container_lighten}`}></div>
+    </div>
+  );
 };
 
 export default Loading;
