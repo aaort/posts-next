74ce76abb11b61126e8fc8d8c45081998f61263e
aort <artashesano@gmail.com>
2023-07-21 13:00:36 +0400

    Remove unused import in posts/loading

--- src/app/posts/loading.tsx
+++ src/app/posts/loading.tsx
@@ -1,7 +1,7 @@
-import LoadingSpinner from '@/components/Loading';
 import styles from '@/styles/components/Card.module.scss';
 import '@/styles/utils/classes.scss';
 
+// Render feature content skeleton
 const Loading: React.FC<React.PropsWithChildren> = () => {
   return (
     <div className="column align_items_center gap_4">
