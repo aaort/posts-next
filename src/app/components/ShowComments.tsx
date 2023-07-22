d00aae8d24c127770bc63a436b49e80f7c9c8b5e
aort <artashesano@gmail.com>
2023-07-22 14:33:17 +0400

    Add explanatory comment in ShowComments

--- src/app/components/ShowComments.tsx
+++ src/app/components/ShowComments.tsx
@@ -5,6 +5,11 @@ import { useState } from 'react';
 
 type Props = React.PropsWithChildren & {};
 
+// ShowComments component includes button for showing/hiding comments
+// as well as <Comments /> components itself, which's taken as props
+// in order to be able to render it as a Server Component which
+// would not be possible otherwise, since Server Components cannot be
+// imported in the Client Components modules. read more: https://nextjs.org/docs/getting-started/react-essentials#the-use-client-directive
 const ShowComments: React.FC<Props> = ({ children }) => {
   const [showComments, setShowComments] = useState<boolean>(false);
 
